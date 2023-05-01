342db0405ee800d575ee3e72135801baaca33eec
yasinsezerturk <yasinsezerturk@gmail.com>
2023-05-01 22:58:18 +0300

    01.05.23 eklendi

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -6,6 +6,7 @@ import ProductList from './productList/ProductList'
 import useFetchCollection from '../../customHooks/useFetchCollection'
 import { useDispatch, useSelector } from 'react-redux'
 import { STORE_PRODUCTS, selectProducts } from '../../redux/slice/productSlice'
+import spinnerImg from "../../assets/spinner.gif"
 
 
 const Product = () => {
@@ -26,10 +27,11 @@ const Product = () => {
     <section>
       <div className={`container ${styles.product}`}>
         <aside className={styles.filter}>
-          <ProductFilter/>
+         {isLoading ? null : <ProductFilter/>} 
         </aside>
         <div className={styles.content}>
-          <ProductList products={products}/>
+         {isLoading ? (<img src={spinnerImg} alt='Loading..' 
+         style={{width:"50px"}}className="--center-all"/>) : <ProductList products={products}/>} 
         </div>
       </div>
     </section>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,21 @@
 //// slider ve product componentinin gösterildiği home sayfasıdır.
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from '../../components/slider/Slider'
 import Product from '../../components/product/Product'
 
 const Home = () => {
+  const url =window.location.href
+  useEffect(()=>{
+    const scrollToProducts=()=>{
+      if(url.includes("#product")){
+        window.scrollTo
+        ({top:700,
+        bahevior:"smooth"})
+        return;
+      }
+    }
+    scrollToProducts()
+  },[url])
   return (
     <div>
       <Slider/>
